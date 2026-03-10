42f61a70700454ef5cbf2b86006ae8a830fd3328
이승환 <seaung13@gmail.com>
2026-03-10 12:06:54 +0900

    [S14P21C103-45] feat: 스프링 부트 프로젝트 초기 설정 및 추가

---
+++ backend/src/main/java/io/ssafy/p/j14c103/homerun/HomerunApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.j14c103.homerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomerunApplication.class, args);
+    }
+
+}
