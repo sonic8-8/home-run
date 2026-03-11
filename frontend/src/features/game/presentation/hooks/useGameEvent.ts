5441cd64fd0589ab3a815ab6142fb180d885efcc
이화목 <ghkahr1890@gmail.com>
2026-03-11 15:49:53 +0900

    [FE] feat: 게임 이벤트 상태관리(hook) (S14P21C103-108)

---
+++ frontend/src/features/game/presentation/hooks/useGameEvent.ts
@@ -0,0 +1,39 @@
+import { useState, useCallback } from 'react';
+import type { GameEvent, GameEventId } from '../../domain/entities/GameEvent';
+import { GAME_EVENTS } from '../constants/gameEvents';
+
+interface UseGameEventReturn {
+  currentEvent: GameEvent | null;
+  showEvent: (eventId: GameEventId) => void;
+  hideEvent: () => void;
+  handleAction: (actionId: string) => void;
+}
+
+export const useGameEvent = (): UseGameEventReturn => {
+  const [currentEvent, setCurrentEvent] = useState<GameEvent | null>(null);
+
+  const showEvent = useCallback((eventId: GameEventId) => {
+    setCurrentEvent(GAME_EVENTS[eventId]);
+  }, []);
+
+  const hideEvent = useCallback(() => {
+    setCurrentEvent(null);
+  }, []);
+
+  const handleAction = useCallback((actionId: string) => {
+    switch (actionId) {
+      case 'RECEIVE':
+        // TODO: 선물 수령 처리 (UseCase 연결)
+        break;
+      case 'APPLY':
+        // TODO: 지원금 신청 처리 (UseCase 연결)
+        break;
+      case 'REJECT':
+        // TODO: 거절 처리 (UseCase 연결)
+        break;
+    }
+    hideEvent();
+  }, [hideEvent]);
+
+  return { currentEvent, showEvent, hideEvent, handleAction };
+};
