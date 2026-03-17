cf53d9b2ca065ee3434f0e88853d209bf51ea445
김은서 <kkkks9203@naver.com>
2026-03-17 11:30:34 +0900

    [BE] refactor(world): 기존 문서 주석 추가, 변수명 변경 (S14P21C103-53)

--- backend/src/main/java/io/ssafy/p/j14c103/homerun/domain/world/housing/HousingType.java
+++ backend/src/main/java/io/ssafy/p/j14c103/homerun/domain/world/housing/HousingType.java
@@ -1,9 +1,14 @@
 package io.ssafy.p.j14c103.homerun.domain.world.housing;
 
 public enum HousingType {
+    // 주거가 없는 상태
     NONE,
+    // 원룸 거주 상태
     STUDIO,
+    // 빌라 거주 상태
     VILLA,
+    // 아파트 전세 거주 상태
     JEONSE_APT,
+    // 자가 아파트 보유 상태
     OWNED_APT
 }
